8c3a6b138c6caf08bf80a98254f24eb80173a063
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-01-29 19:55:16 +0530

    using ReactElement() to create components

--- Lec 1/index.js
+++ Lec 1/index.js
@@ -17,21 +17,98 @@ const heading2 = React.createElement(
   "I am Ayush Shrivastav"
 );
 
+const heading3 = React.createElement(
+    "h3",
+    {
+      id: "title",
+      style:{
+        color:"cyan"
+      }
+    },
+    "List of Fruits"
+  );
 const container = React.createElement(
   "div",
   {
     id: "container",
     style:{
-        border:"5px solid cyan",
-        display:"flex",
-        flexDirection:"column",
-        padding:"20px"
+
     }
   },
   [heading, heading2]
 ); //passing Array of element, Wraping them in one container
 
+
+const container2 = React.createElement(
+    "div",
+    {
+        id:"container-2",
+        style:{
+            backgroundColor:"black",
+            color:"white",
+            border :"2px solid red",
+            border:"5px solid cyan",
+            display:"flex",
+            flexDirection:"column",
+            padding:"20px"
+        }
+    },
+    [
+        heading,
+        heading2,
+        heading3,
+        React.createElement(
+            "ul",
+            {
+              id: "ul-1",
+              style:{
+                color:"yellow"
+              }
+            },
+            [
+            React.createElement(
+                "li",
+                {
+                  id: "li-1",
+                  style:{
+                  }
+                },
+                "Mango"
+              ),
+              React.createElement(
+                "li",
+                {
+                  id: "li-2",
+                  style:{
+                  }
+                },
+                "grape"
+              ),
+              React.createElement(
+                "li",
+                {
+                  id: "li-3",
+                  style:{
+                  }
+                },
+                "papaya"
+              ), 
+              React.createElement(
+                "li",
+                {
+                  id: "li-4",
+                  style:{
+                  }
+                },
+                "Apple"
+              ),              
+            ]
+          )
+    ]
+);
+
+
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //passing a react element inside the root
-root.render(container);
+root.render(container2);
