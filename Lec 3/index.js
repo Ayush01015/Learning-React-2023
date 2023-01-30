776348d19ade7e500a03c0fea7ddf57c64ffd666
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-01-30 14:14:00 +0530

    Final css added

--- Lec 3/index.js
+++ Lec 3/index.js
@@ -1,38 +1,41 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 const Header = () => {
-    return(
-        <div
-        className="font"
+  return (
+    <div
+      className="font"
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "20px",
+        margin: "auto auto",
+        fontSize: "1.3rem",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        fontWeight: "bold",
+      }}
+    >
+      <span style={{ cursor: "pointer" }}>LOGO</span>
+      <ul
         style={{
-            
-
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          listStyle: "none",
+          marginLeft: "50rem",
         }}
-        >
-            <span style={{cursor:"pointer"}}>LOGO</span>
-            <ul style={{
-                
-
-                
-            }}>
-                <li
-                className="li-item"
-                >Home</li>
-                <li
-                className="li-item"
-                >About</li>
-                <li
-                className="li-item"
-                >Contact Us</li>
-                <li
-                className="li-item"
-                >Shop</li>
-            </ul>
-        </div>
-    )
-}
+      >
+        <li className="li-item">Home</li>
+        <li className="li-item">About</li>
+        <li className="li-item">Contact Us</li>
+        <li className="li-item">Shop</li>
+      </ul>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Header/>);
+root.render(<Header />);
