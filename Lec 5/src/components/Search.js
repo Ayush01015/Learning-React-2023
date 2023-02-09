a5e40ee65ae6b6fbb8019c45dc30788122a792b4
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-09 16:06:39 +0530

    Hard Coded Value for Search With Don't Work

--- Lec 5/src/components/Search.js
+++ Lec 5/src/components/Search.js
@@ -1,9 +1,9 @@
 
-
+const value = "Ayush";
 const Search = () => {
     return(
         <div className="search-component">
-        <input className="search-input" type="text" placeholder="Search..." value=""/>
+        <input className="search-input" type="text" placeholder="Search..." value={value}/>
         <button className="search-btn">Search</button>
         </div>
     )
