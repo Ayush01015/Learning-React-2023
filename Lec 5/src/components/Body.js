796c326396110abef268256af695385073e68c58
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-10 15:47:39 +0530

    Leec 5 Comments added

--- Lec 5/src/components/Body.js
+++ Lec 5/src/components/Body.js
@@ -1,4 +1,3 @@
-
 import Card from "../components/Card";
 import { useState } from "react";
 import { restaurantsList } from "../constants";
@@ -36,7 +35,7 @@ const Body = () => {
           Search
         </button>
       </div>
-    <div className="body">
+      <div className="body">
         {restaurants.map((restaurant) => {
           return <Card {...restaurant.data} key={restaurant.data.uuid} />;
         })}
@@ -47,3 +46,22 @@ const Body = () => {
 
 export default Body;
 
+/*
+    const [searchInput,setSearchInput] = useState("");
+    # It is a useState Hook used to declare a local state variable.
+    # Hooks are nothing but just a JS function
+    # Here useState return an Array
+    # And in returned Array index[0] is Variable name
+    # And index[1] is function which is used to change the value of     state variable.
+    # const [searchInput,setSearchInput] = useState("");
+    # const [     0     ,     1        ] = useState(defaultvalue);
+    */
+/*
+        value={searchInput} //default "" --> empty String
+        onChange={(e)=>setSearchInput(e.target.value)}
+        # Here setSearchInput() function is taking e.target.value
+        as Argument. 
+        # setSearhInput() changes the state to whatever
+        there is in e.target.value
+        # setSearchInput() is used to change the local state variable.
+        */
