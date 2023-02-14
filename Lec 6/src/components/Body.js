7479662e898f7c11fdaf613a3d8bd7375f5a8b95
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-14 11:33:01 +0530

    Search working for fitered Restaurants

--- Lec 6/src/components/Body.js
+++ Lec 6/src/components/Body.js
@@ -1,50 +1,37 @@
 import Card from "../components/Card";
 import { useState, useEffect } from "react";
-import { restaurantsList } from "../constants";
+// import { restaurantsList } from "../constants";
 import { SWIGGY_PUBLIC_API } from "../constants";
 import ShimmerUI from "./ShimmerUI";
-// import Search from "./Search";
-/*
-const [searchInput,setSearchInput] = useState("");
-# It is a useState Hook used to declare a local state variable.
-# Hooks are nothing but just a JS function
-# Here useState return an Array
-# And in returned Array index[0] is Variable name
-# And index[1] is function which is used to change the value of     state variable.
-# const [searchInput,setSearchInput] = useState("");
-# const [     0     ,     1        ] = useState(defaultvalue);
-*/
-/*
-value={searchInput} //default "" --> empty String
-onChange={(e)=>setSearchInput(e.target.value)}
-# Here setSearchInput() function is taking e.target.value
-as Argument. 
-# setSearhInput() changes the state to whatever
-there is in e.target.value
-# setSearchInput() is used to change the local state variable.
-*/
-function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
+function filterData(searchText, allRestaurants) {
+   //filtering data from allRestaurants
+  const filterData = allRestaurants.filter((restaurant) =>
     restaurant.data.name.includes(searchText)
   );
 
-  return filterData;
+  return filterData; //returning searched data
 }
 const Body = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const [restaurants, setRestaurants] = useState([]);
-  // console.log(restaurants);
-  async function getRestaurants() {
+  const [searchInput, setSearchInput] = useState(""); //for searching input in seach input box
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]); // for searched data on search button
+  const [allRestaurants, setAllRestaurants] = useState([]); // for rendering all data from API. 
+
+
+  async function getRestaurants() { //fetching data fromm API
     const data = await fetch(SWIGGY_PUBLIC_API);
     const json = await data.json();
-    // console.log("Data: ", data);
     console.log("Json: ", json);
-    setRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards); //Setting data in restaurants
+    setfilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards); //Setting data in filtered restaurants for search.
   }
+
+
   useEffect(() => {
     getRestaurants();
   }, []);
-  return restaurants?.length === 0 ? (
+
+
+  return filteredRestaurants?.length === 0 ? (
     <div
       style={{
         display:"flex",
@@ -83,9 +70,9 @@ const Body = () => {
           className="search-btn"
           onClick={() => {
             //need to filter the data
-            const data = filterData(searchInput, restaurants);
+            const data = filterData(searchInput, allRestaurants);
             // update the state - restaurants
-            setRestaurants(data);
+            setfilteredRestaurants(data);
             // setSearchInput("Clicked");
           }}
         >
@@ -93,7 +80,7 @@ const Body = () => {
         </button>
       </div>
       <div className="body">
-        {restaurants.map((restaurant) => {
+        {filteredRestaurants.map((restaurant) => {
           return <Card {...restaurant.data} key={restaurant.data.uuid} />;
         })}
       </div>
