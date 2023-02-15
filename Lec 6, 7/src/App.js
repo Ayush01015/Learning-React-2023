cc921ebaefe64fdc16dac709cd848748d485bc46
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-16 02:26:01 +0530

    Routing Added, AboutUS Component Created

--- Lec 6, 7/src/App.js
+++ Lec 6, 7/src/App.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 const SwiGker = () => {
   return (
     <>
@@ -13,6 +18,17 @@ const SwiGker = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<SwiGker/>
+  },
+  {
+    path:"/about",
+    element:<AboutUs/>
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<SwiGker />);
+root.render(<RouterProvider router={appRouter}/>);
---
+++ Lec 6, 7/src/components/AboutUs.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta eum
+        nihil natus deleniti tempora aliquam odit eligendi, illum doloremque ea
+        fuga nemo! Sit consequatur laborum eos dolor tempora earum temporibus
+        blanditiis molestias et, officia velit tenetur eum repudiandae? Quidem
+        molestiae, fugiat corrupti qui ut dicta animi architecto nam iure totam.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
