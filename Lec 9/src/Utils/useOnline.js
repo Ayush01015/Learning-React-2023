2b902c23b9ea34d1bfa24bf14895595dd1b0d5c8
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-23 22:28:50 +0530

    useOnline Hook Created

---
+++ Lec 9/src/Utils/useOnline.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from "react";
+
+const useOnline = () => {
+
+    const [isOnline,setIsOnline] = useState(true);
+
+    useEffect(()=>{
+        window.addEventListener("online",()=>{
+            setIsOnline(true);
+        })
+        window.addEventListener("offline",()=>{
+            setIsOnline(false);
+        })
+    },[])
+
+    return isOnline;
+}
+
+export default useOnline;
