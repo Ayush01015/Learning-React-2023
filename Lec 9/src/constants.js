6c5f6890af8edb9b04a811c171cc91fbe4e56973
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-02-25 23:01:54 +0530

    save

--- Lec 9/src/constants.js
+++ Lec 9/src/constants.js
@@ -10,19 +10,12 @@ export const SWIGGY_PUBLIC_API =
 export const SWIGGY_MENU_API =
   "https://www.swiggy.com/dapi/menu/v4/full?lat=28.627981&lng=77.3648567&menuId=";   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// export const IMG_CDN_URL =
+//   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+// export const SWIGGY_PUBLIC_API =
+//   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.627981&lng=77.3648567&page_type=DESKTOP_WEB_LISTING";
+// export const SWIGGY_MENU_API =
+//   "https://www.swiggy.com/dapi/menu/v4/full?lat=28.627981&lng=77.3648567&menuId=";   
 
 
 
