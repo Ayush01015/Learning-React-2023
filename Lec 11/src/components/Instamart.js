acdcfbd858c692b9a27b7bc7328cdb204680e4bd
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-03 22:58:53 +0530

    Shit Code

--- Lec 11/src/components/Instamart.js
+++ Lec 11/src/components/Instamart.js
@@ -1,30 +1,34 @@
 import React, { useState } from "react";
-const Section = ({ title, description }) => {
-  const [isVisible, setIsVisible] = useState(false);
+const Section = ({ title, description, isVisible, setIsVisible }) => {
   return (
     <div className="p-3 m-2 border-2 border-black">
       <h3 className="font-bold text-2xl">{title}</h3>
-      {
-        !isVisible && <button
+      {!isVisible && (
+        <button
           className="border border-black p-1 w-[60px] bg-[#f4a545] rounded hover:bg-[#f88f0e]"
           onClick={() => setIsVisible(true)}
         >
           Show
         </button>
-      }
-      {
-        isVisible && <button
+      )}
+      {isVisible && (
+        <button
           className="border border-black p-1 w-[60px] bg-[#f4a545] rounded hover:bg-[#f88f0e]"
           onClick={() => setIsVisible(false)}
         >
           Hide
         </button>
-      }
+      )}
       {isVisible && <p>{description}</p>}
     </div>
   );
 };
 const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    setAbout: false,
+    setContact: false,
+    setServices: false,
+  });
   return (
     <div>
       <Section
@@ -32,11 +36,27 @@ const Instamart = () => {
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
+        isVisible={sectionConfig.setAbout}
+        setIsVisible={() =>
+          setSectionConfig({
+            setAbout: true,
+            setContact: false,
+            setServices: false,
+          })
+        }
       />
       <Section
         title={"Contact"}
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+        isVisible={sectionConfig.setContact}
+        setIsVisible={() =>
+          setSectionConfig({
+            setAbout: false,
+            setContact: true,
+            setServices: false,
+          })
         }
       />
       <Section
@@ -44,9 +64,23 @@ const Instamart = () => {
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
+        isVisible={sectionConfig.setServices}
+        setIsVisible={() =>
+          setSectionConfig({
+            setAbout: false,
+            setContact: false,
+            setServices: true,
+          })
+        }
       />
     </div>
   );
 };
 
 export default Instamart;
+
+/**
+ * lifting the state up
+ *    taking power(control of state) from child(Section) to parent(Instamart)
+ *
+ */
