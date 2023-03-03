e0bcef1d0ab64916fade943d8b16c2e0f3ec1bd6
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-03 22:03:51 +0530

    Tailwind Config

--- Lec 11/src/components/Instamart.js
+++ Lec 11/src/components/Instamart.js
@@ -1,12 +1,20 @@
 import React from 'react'
-
+const Section = ( {title, description} ) =>{
+  return(
+    <div className='bg-black border-4 border-red-500 text-white'>
+      <h3>{title}</h3>
+      <h3>{description}</h3>
+    </div>
+  )  
+}
 const Instamart = () => {
   return (
     <div>
-      <h1>Instamart</h1>
-      <h1>100's of Components</h1>
+      <Section title={"About"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
+      <Section title={"contact"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
+      <Section title={"other"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
     </div>
   )
 }
 
-export default Instamart
+export default Instamart;
