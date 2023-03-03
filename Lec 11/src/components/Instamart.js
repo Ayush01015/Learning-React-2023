6d5c161caecef71cd26cef006dcebbdf5b0eb105
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-03 22:21:46 +0530

    Button Added

--- Lec 11/src/components/Instamart.js
+++ Lec 11/src/components/Instamart.js
@@ -1,20 +1,52 @@
-import React from 'react'
-const Section = ( {title, description} ) =>{
-  return(
-    <div className='bg-black border-4 border-red-500 text-white'>
-      <h3>{title}</h3>
-      <h3>{description}</h3>
+import React, { useState } from "react";
+const Section = ({ title, description }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="p-3 m-2 border-2 border-black">
+      <h3 className="font-bold text-2xl">{title}</h3>
+      {
+        !isVisible && <button
+          className="border border-black p-1 w-[60px] bg-[#f4a545] rounded hover:bg-[#f88f0e]"
+          onClick={() => setIsVisible(true)}
+        >
+          Show
+        </button>
+      }
+      {
+        isVisible && <button
+          className="border border-black p-1 w-[60px] bg-[#f4a545] rounded hover:bg-[#f88f0e]"
+          onClick={() => setIsVisible(false)}
+        >
+          Hide
+        </button>
+      }
+      {isVisible && <p>{description}</p>}
     </div>
-  )  
-}
+  );
+};
 const Instamart = () => {
   return (
     <div>
-      <Section title={"About"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
-      <Section title={"contact"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
-      <Section title={"other"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
+      <Section
+        title={"About"}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      />
+      <Section
+        title={"Contact"}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      />
+      <Section
+        title={"Services"}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Instamart;
