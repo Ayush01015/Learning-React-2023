e483b426a2351e2606ac8fe3579e9a2d95d57e3c
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-09 14:48:40 +0530

    Exporting actions and reducer [V.Imp]

--- Lec 12/src/Utils/cartSlice.js
+++ Lec 12/src/Utils/cartSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
     name:"cart",
     initialState:{
-        items:[]
+        items:["Banana","Apples"]
     },
     reducers:{
         //these function did not return anything they take the state and directly modify it.
@@ -11,10 +11,14 @@ const cartSlice = createSlice({
             state.items.push(action.payload);
         },
         removeItem:(state)=>{
-            state.items.pop();
+            state.items.pop(); //write proper logic which remove by index
         },
         clearCart:(state)=>{
             state.items = [];
         }
     },
 })
+
+export const {addItem, removeItem, clearCart} = cartSlice.actions;
+export default cartSlice.reducer;
+ 
