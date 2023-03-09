8149307add2cfae426d02eab14c224e950ac73b1
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-09 13:40:29 +0530

    Adding Template for Lec 12

---
+++ Lec 12/src/App.js
@@ -0,0 +1,84 @@
+import React, { lazy, Suspense,useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
+import ErrorPage from "./components/ErrorPage";
+import Contact from "./components/Contact"; 
+// import Instamart from "./components/Instamart";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Deals from "./components/Deals";
+import RestaurantsMenu from "./components/RestaurantsMenu";
+import ShimmerCard from "./components/ShimmerCard";
+import userContext from "./Utils/userContext";
+/*
+Chunking
+code splitting
+dynamic bundling
+lazy loading
+on demand loading
+dynamic import
+(synonyms)
+*/
+const Instamart = lazy(() => import("./components/Instamart.js")); //it's a promise(js)
+
+const SwiGker = () => {
+  const [user,setUser] = useState({
+    name:"Ayush Shrivastav",
+    email:"ayushshrivastav575@gmail.com"
+  });
+  return (
+    <userContext.Provider value={{
+      user:user,
+      setUser:setUser
+    }}>
+      <Header />
+      {/* {every Children Will come at the place of Outlet when the Link is clicked} */}
+      <Outlet />
+      <Footer />
+    </userContext.Provider>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <SwiGker />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/deals",
+        element: <Deals />,
+      },
+      {
+        path: "/restaurants/:id",
+        element: <RestaurantsMenu />,
+      },
+      {
+        path: "/instamart",
+        element: (
+          <Suspense fallback={<ShimmerCard/>}>
+            <Instamart />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
---
+++ Lec 12/src/components/Header.js
@@ -0,0 +1,82 @@
+import { useState,useContext } from "react";
+import { Link } from "react-router-dom";
+import useOnline from "../Utils/useOnline";
+const Header = () => {
+  const [log, setLog] = useState(true);
+  const isOnline = useOnline();
+  return (
+    <div
+      className="font"
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "22px",
+        fontSize: "1.3rem",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        fontWeight: "bold",
+        // justifyContent:"space-around"
+      }}
+    >
+      <span style={{ cursor: "pointer", fontSize: "2rem" }}>
+      <Link to="/" > swi<span style={{ color: "#F99417" }}>G</span>ker</Link>
+      </span>
+
+      {
+        (!isOnline)?(
+          <span
+        style={{
+          position: "absolute",
+          right: "4.5rem",
+        }}
+      >{`🚫`}</span>
+        ):(
+          <span
+        style={{
+          position: "absolute",
+          right: "4.5rem",
+        }}
+      >{`🟢`}</span>
+        )
+
+      }
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          listStyle: "none",
+          marginLeft: "30rem",
+        }}
+      >
+        {/* have to add pseudo class active for 1st item* */}
+        <li className="li-item" style={{ color: "#F99417" }}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className="li-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="li-item">
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li className="li-item">
+          <Link to="/deals">Deals</Link>
+        </li>
+        <li className="li-item">
+          <Link to="/instamart">Instamart</Link>
+        </li>
+      </ul>
+      {log === true ? (
+        <button className="log-btn" onClick={() => setLog(false)}>
+          Log in
+        </button>
+      ) : (
+        <button className="log-btn" onClick={() => setLog(true)}>
+          Log out
+        </button>
+      )}
+    </div>
+  );
+};
+export default Header;
