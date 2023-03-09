ca98d54bee9c8f5c97fd6d84978af7bea812a8ef
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-03-09 14:35:57 +0530

    Adding removeItem to slice [Cart]

--- Lec 12/src/Utils/cartSlice.js
+++ Lec 12/src/Utils/cartSlice.js
@@ -10,6 +10,9 @@ const cartSlice = createSlice({
         addItem:(state,action)=>{
             state.items.push(action.payload);
         },
+        removeItem:(state)=>{
+            state.items.pop();
+        },
         clearCart:(state)=>{
             state.items = [];
         }
